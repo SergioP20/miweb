9da86728de1ef216169158f24666fd810d0012a8
Sergio Padilla <sergiopa2097@gmail.com>
2022-09-25 14:43:13 -0500

    Cargando programa funcional

---
+++ src/Corresponsal.java
@@ -0,0 +1,11 @@
+public class Corresponsal {
+    String message_A(String nom,String msj){
+
+        if (nom == "Jhonny"){
+            mjs = "Hola mi amor";
+        }
+        else
+            msj = msj + nom;
+        return mjs;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Creemos un saludo!    Ingresa tu nombre");
+        String name = "";
+        Scanner n = new Scanner(System.in);
+        name = n.nextLine();
 
-        System.out.println("Hello world!");
+        Corresponsal obj = new Corresponsal();
+
+        obj.message_A(name,"Hola");
+
     }
 }
